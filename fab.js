19986dddfaf5c02f6a704084eed074b253a2ef54
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-29 05:28:19 +0000

    edit viewport vh blur

--- fab.js
+++ fab.js
@@ -9,7 +9,7 @@ class Blur {
             position: absolute;
             top: 0;
             left: 0;
-            height: 500vh;
+            height: 800vh;
             width: 100vw;
             backdrop-filter: blur(8px);
             z-index: 1001;
